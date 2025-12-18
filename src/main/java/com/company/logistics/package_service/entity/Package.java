439b3b896d7ae93ed96a1c326ff83c0bc20670d8
zidane2001 <zidane2001@github.com>
2025-12-18 18:08:13 +0100

    refactor(Package.java): add ommited @positive constraint to field weight

--- src/main/java/com/company/logistics/package_service/entity/Package.java
+++ src/main/java/com/company/logistics/package_service/entity/Package.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Entity
@@ -30,6 +31,7 @@ public class Package {
 
     @Column(name = "weight")
     @NotNull(message = "Weight is mandatory")
+    @Positive(message = "Weight must be positive")
     @Max(value = 10000, message = "Weight must not exceed 10000")
     private Double weight;
 
